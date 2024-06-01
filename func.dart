c2e0284ec06bbc6c87c338d1c7ef494693297812
Joseph <indiekaj@gmail.com>
2024-06-01 18:49:58 +0300

    some dart

--- func.dart
+++ func.dart
@@ -1,5 +1,7 @@
 addNum(int a, int b) => a + b;
 
+double multi({int a=12, required double c}) => a * c;
+
 int getFirstElement(List<int> lst) => lst[0];
 
 int stringLength(String name) => name.length;
@@ -8,6 +10,7 @@ void main() {
   print("Hello");
   var sum = addNum(12, 45);
   print(sum);
+  print(multi(c: 2, a: 14));
   var ls = [12, 34, 45, 64];
   int element = getFirstElement(ls);
   print(element);
