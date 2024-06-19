a78b177ae474eddff7248ccc8166650667d75cef
Joseph <indiekaj@gmail.com>
2024-06-19 11:24:25 +0300

    arrays

--- java_s/foreach.js
+++ java_s/foreach.js
@@ -13,4 +13,30 @@ numbers.forEach(adder)
 let double = numbers.map(num => num*2);
 //map will not modify the new array but the for each can modify the new array
 
-console.log(double);
+//console.log(double);
+
+//example 2 on map
+let people = [
+    {firstname: "Joseph", secondName:"Indieka", age: 20},
+    {firstname: "John", secondName:"Ali", age: 18},
+    {firstname: "Karen", secondName:"Mammk", age: 23},
+    {firstname: "Bradely", secondName:"Kanyari", age: 15}
+]
+
+const results = people.map(person => {
+    return [
+        person.firstname.toLowerCase(), 
+        person.secondName.toUpperCase(), 
+        person.age <= 18 ? 'Underage' : "Above age"
+    ]
+})
+
+//console.log(results);
+
+//challenge
+function multi(num){
+   return num * 10;
+} 
+const newNumbers = numbers.map(multi)
+
+console.log(newNumbers);
