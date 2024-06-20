de6d43ae279c53bac39a7cdb1c5aa1f4a3ae34f3
Joseph <indiekaj@gmail.com>
2024-06-20 23:06:42 +0300

    fixes

--- dart/func.dart
+++ dart/func.dart
@@ -1,10 +1,17 @@
+//add two numbers
 addNum(int a, int b) => a + b;
 
+//divide two numbers and return a double
 double multi({int a=12, required double c}) => a * c;
 
+//get first element in the list of integers
 int getFirstElement(List<int> lst) => lst[0];
 
+//returns the lenth of the string parsed
 int stringLength(String name) => name.length;
+
+//checks if the number is even or not
+bool checkeven(int num) => num % 2 == 0 ? true : false;
 
 void main() {
   print("Hello");
@@ -16,4 +23,5 @@ void main() {
   print(element);
   int len = stringLength('Liz');
   print(len);
+  print(checkeven(22));
 }
