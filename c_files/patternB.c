6134b9b9ec9751eec8eac3a6f51c953709de7778
Joseph <indiekaj@gmail.com>
2024-09-02 22:18:19 +0300

    pattern2

---
+++ c_files/patternB.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main () {
+    for (int i = 1; i <= 7; i++){
+        for (int j = 1; j <= 7; j++){
+            if((j==1) || (i==1 && j<=6) || (i==4 && j<=6) || (i==7 && j<=6) || (j==7 && i>1 && i<4) || (j==7 && i>4 && i<7)){
+                printf("+");
+            } else {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
