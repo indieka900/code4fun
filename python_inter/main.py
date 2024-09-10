4378460849846e49c1721fdb45b89c72627a2de4
Joseph <indiekaj@gmail.com>
2024-09-10 23:55:52 +0300

    progress bar and indicataor

--- python_inter/main.py
+++ python_inter/main.py
@@ -6,7 +6,7 @@ def downloadV():
     try:
         finishedLabel.configure(text="")
         ytlink = link.get()
-        ytObject = YouTube(ytlink)
+        ytObject = YouTube(ytlink, on_progress_callback=on_progress)
         video = ytObject.streams.get_highest_resolution()
         title.configure(text=ytObject.title, text_color = "white")
         video.download()
@@ -14,6 +14,19 @@ def downloadV():
         finishedLabel.configure(text="Download Error!", text_color="red")
         return
     finishedLabel.configure(text="Downloaded!")
+
+def on_progress(stream, chunk, bytes_remaining):
+    total_size = stream.filesize
+    bytes_downloaded = total_size - bytes_remaining
+    percentage_complition = bytes_downloaded / total_size * 100
+    per = str(int(percentage_complition))
+    pPercentage.configure(text=per+'%')
+    pPercentage.update()
+    
+    #update progress bar
+    progressBar.set(float(percentage_complition) / 100)
+    progressBar.update()
+    
 
 #Systems Settings
 customtkinter.set_appearance_mode("System")
@@ -36,6 +49,15 @@ link.pack()
 #download button
 downloadB = customtkinter.CTkButton(app, text="Download", command=downloadV)
 downloadB.pack(padx=10, pady=10)
+
+#progress percentage
+pPercentage = customtkinter.CTkLabel(app, text="0%")
+pPercentage.pack(pady=20)
+
+#progress bar
+progressBar = customtkinter.CTkProgressBar(app, width=400)
+progressBar.set(0.0)
+progressBar.pack(pady=10)
 
 #finished downloading
 finishedLabel = customtkinter.CTkLabel(app, text="")
